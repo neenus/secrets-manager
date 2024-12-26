78af902dc9aca360a33ff0915c38941101460155
Neenus Gabriel <hello@neenus.com>
2024-12-26 14:37:02 +0000

    feat(user.ts, package.json): add bcrypt to hash user passwords before saving into DB

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Document } from "mongoose";
+import bcrypt from "bcryptjs";
 
 export interface IUser extends Document {
   email: string;
@@ -6,10 +7,19 @@ export interface IUser extends Document {
   apiKey: string;
 }
 
-const userSchema: Schema<IUser> = new Schema({
+const UserSchema: Schema<IUser> = new Schema({
   email: { type: String, required: [true, "Please add an email"], unique: true, validate: { validator: (v: string) => /\S+@\S+\.\S+/.test(v), message: "Please add a valid email" }, index: true },
   password: { type: String, required: [true, "Please add a password"], validate: { validator: (v: string) => v.length >= 6, message: "Password must be at least 6 characters long" }, index: true },
   apiKey: { type: String, required: [true, "Please add a user API key"], unique: true },
 });
 
-export default mongoose.model<IUser>("User", userSchema);
+// Hash the password before saving the user model
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  const user = this as IUser;
+  if (!user.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+export default mongoose.model<IUser>("User", UserSchema);
