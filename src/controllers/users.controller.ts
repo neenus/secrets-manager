041cceb163a21d4b446bcea31f2b553ca382d61c
Neenus Gabriel <hello@neenus.com>
2024-12-27 07:34:11 +0000

    refactor(users.controller.ts): add apiKeyIdentifier to new users registered

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -6,6 +6,7 @@ import crypto from "crypto";
 export const register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const { email, password } = req.body;
   const apiKey = crypto.randomBytes(24).toString("hex");
+  const apiKeyIdentifier = crypto.createHash("sha256").update(apiKey).digest("hex").substring(0, 16);
 
   // check if password is at least 6 characters long
   if (password.length < 6) return next(new ErrorResponse("Password must be at least 6 characters long", 400));
@@ -14,7 +15,7 @@ export const register = async (req: Request, res: Response, next: NextFunction):
     const user = await User.findOne({ email });
     if (user) return next(new ErrorResponse("User already exists", 400));
 
-    const newUser = new User({ email, password, apiKey });
+    const newUser = new User({ email, password, apiKey, apiKeyIdentifier });
     await newUser.save();
     res.status(201).json({ success: true, data: newUser });
   } catch (err: any) {
