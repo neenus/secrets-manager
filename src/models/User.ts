7f2b8a524cb552614fac96c4b8459e2e34e79a11
Neenus Gabriel <hello@neenus.com>
2024-12-27 07:31:06 +0000

    refactor(user.ts): add apiKeyIdentifier field to User schema
    apiKeyIdentifier will be use to query the users table

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,6 +6,7 @@ export interface IUser extends Document {
   email: string;
   password: string;
   apiKey: string;
+  apiKeyIdentifier: string;
   compareHash(hashValue: string, hashType: "password" | "apiKey"): Promise<boolean>;
 }
 
@@ -13,6 +14,7 @@ const UserSchema: Schema<IUser> = new Schema({
   email: { type: String, required: [true, "Please add an email"], unique: true, validate: { validator: (v: string) => /\S+@\S+\.\S+/.test(v), message: "Please add a valid email" }, index: true },
   password: { type: String, required: [true, "Please add a password"], validate: { validator: (v: string) => v.length >= 6, message: "Password must be at least 6 characters long" }, index: true },
   apiKey: { type: String, required: [true, "Please add a user API key"], unique: true },
+  apiKeyIdentifier: { type: String, required: true, unique: true }
 });
 
 // Hash the password and API key before saving the user model
