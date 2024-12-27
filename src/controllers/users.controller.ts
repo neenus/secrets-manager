4b5033e04acb7f50030277a534dfb7f8c0359309
Neenus Gabriel <hello@neenus.com>
2024-12-27 04:20:20 +0000

    refactor(users.controller.ts): refactor users controller to use bcrypt to hash apiKey
    apiKey hashing is happenening in mongoose pre save hook

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from "express";
 import User from "@models/User";
-import { encrypt } from "@utils/encryption";
 import ErrorResponse from "@utils/errorResponse";
 import crypto from "crypto";
 
@@ -11,17 +10,14 @@ export const register = async (req: Request, res: Response, next: NextFunction):
   // check if password is at least 6 characters long
   if (password.length < 6) return next(new ErrorResponse("Password must be at least 6 characters long", 400));
 
-  // encrypt and store the API key
-  const encryptedApiKey = encrypt(apiKey);
-
   try {
     const user = await User.findOne({ email });
     if (user) return next(new ErrorResponse("User already exists", 400));
 
-    const newUser = new User({ email, password: password, apiKey: encryptedApiKey });
+    const newUser = new User({ email, password, apiKey });
     await newUser.save();
     res.status(201).json({ success: true, data: newUser });
   } catch (err: any) {
     return next(new ErrorResponse("Internal server error", err.statusCode || 500));
   }
-}
+};
