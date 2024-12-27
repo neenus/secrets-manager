4cca341ec774d24d07bcf5be1309159e0e4fe997
Neenus Gabriel <hello@neenus.com>
2024-12-27 04:11:25 +0000

    feat(user.ts): refactor pre-save hook in user mode and add compareHash method

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,12 @@
 import mongoose, { Schema, Document } from "mongoose";
 import bcrypt from "bcryptjs";
+import ErrorResponse from "@utils/errorResponse";
 
 export interface IUser extends Document {
   email: string;
   password: string;
   apiKey: string;
+  compareHash(hashValue: string, hashType: "password" | "apiKey"): Promise<boolean>;
 }
 
 const UserSchema: Schema<IUser> = new Schema({
@@ -13,13 +15,24 @@ const UserSchema: Schema<IUser> = new Schema({
   apiKey: { type: String, required: [true, "Please add a user API key"], unique: true },
 });
 
-// Hash the password before saving the user model
+// Hash the password and API key before saving the user model
 UserSchema.pre("save", async function (next) {
-  const salt = await bcrypt.genSalt(10);
   const user = this as IUser;
-  if (!user.isModified("password")) return;
-  this.password = await bcrypt.hash(this.password, salt);
+  if (user.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+  }
+  if (user.isModified("apiKey")) {
+    const salt = await bcrypt.genSalt(10);
+    user.apiKey = await bcrypt.hash(user.apiKey, salt);
+  }
   next();
 });
+
+// Validate user password and API key and compare it to the hashed value in the database
+UserSchema.methods.compareHash = async function (hashValue: string, hashType: "password" | "apiKey"): Promise<boolean> {
+  if (hashType && !this[hashType]) throw new ErrorResponse(`${hashType} is not set`, 400);
+  return await bcrypt.compare(hashValue, this[hashType]);
+}
 
 export default mongoose.model<IUser>("User", UserSchema);
